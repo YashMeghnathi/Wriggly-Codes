789ae2e470a89a1ac79ec7ee3138ed8bb67ad590
Yash Meghnathi <125940745+YashMeghnathi@users.noreply.github.com>
2025-02-08 18:33:35 +0530

    Update Convert Ether.sol

--- contracts/Codes(EASY)/Convert Ether.sol
+++ contracts/Codes(EASY)/Convert Ether.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 contract EtherConverter {
     // Function to convert Ether to Wei
     function etherToWei(uint256 etherAmount) public pure returns (uint256) {
-        return etherAmount * 10**18; // 1 Ether = 10^18 Wei
+        return etherAmount * 10**18; // 
     }
     
     // Function to convert Wei to Ether
     function weiToEther(uint256 weiAmount) public pure returns (uint256) {
-        return weiAmount / 10**18; // 1 Wei = 10^-18 Ether
+        return weiAmount / 10**18; // 
     }
 }
