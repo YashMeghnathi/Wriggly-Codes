5f02a84ad25555379dbd78990cd57f772ba48c1f
Yash Meghnathi <125940745+YashMeghnathi@users.noreply.github.com>
2025-02-08 18:28:41 +0530

    Update Concatenate.sol

--- contracts/Codes(EASY)/Concatenate.sol
+++ contracts/Codes(EASY)/Concatenate.sol
@@ -7,4 +7,4 @@ contract StringConcatenation {
     function concat(string memory str1, string memory str2) public pure returns (string memory) {
         return string(abi.encodePacked(str1, str2));
     }
-}
+
