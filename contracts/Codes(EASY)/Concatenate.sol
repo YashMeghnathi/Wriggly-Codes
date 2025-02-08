f77bda322a2514bc6c268d77125aa4a3a0f83840
Yash Meghnathi <125940745+YashMeghnathi@users.noreply.github.com>
2025-02-08 18:32:40 +0530

    Update Concatenate.sol

--- contracts/Codes(EASY)/Concatenate.sol
+++ contracts/Codes(EASY)/Concatenate.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract StringConcatenation {
-
-    // Function to concatenate two strings
     function concat(string memory str1, string memory str2) public pure returns (string memory) {
         return string(abi.encodePacked(str1, str2));
     }
